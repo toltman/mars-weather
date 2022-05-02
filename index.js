cae9898ac6bed4712b3ec0104d6e267ad1ce59ce
Tim Oltman <toltman@gmail.com>
2022-05-02 12:55:28 -0500

    changing codesandbox config to work here

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import App from "./App.svelte";
-import "./public/bundle.css";
 
 const app = new App({
   target: document.getElementById('app'),
--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
     sourcemap: true,
     format: "iife",
     name: "app",
-    file: "public/bundle.js"
+    file: "public/index.js"
   },
   plugins: [
     svelte({
@@ -23,7 +23,7 @@ export default {
       // we'll extract any component CSS out into
       // a separate file — better for performance
       css: css => {
-        css.write("public/bundle.css");
+        css.write("bundle.css");
       }
     }),
 
